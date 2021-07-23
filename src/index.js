eed3c97676741a62a390085bf316b6d5a9354e44
ttakuma17 <76837734+ttakuma17@users.noreply.github.com>
2021-07-23 22:48:42 +0900

    commit on section5-32

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const InputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  // console.log(InputText);
+
+  //div作成
+  const div = document.createElement("div");
+  div.className = "list-raw";
+
+  //li作成
+  const li = document.createElement("li");
+  li.innerText = InputText;
+  // console.log(li);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  // console.log(div);
+
+  //未完了のリストに追加　＊　なぜか1つ目のリストの間にテキストがはいり動画と挙動が違うようにみえる
+  document.getElementById("incomplete-list").appendChild(div);
+
+};
+
+document
+.getElementById("add-button")
+.addEventListener("click", () => onClickAdd());
+
